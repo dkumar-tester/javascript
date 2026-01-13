bb56ddae09f0fe250646b38bc164da075d10b1e0
dkumar-tester <dkumar.tester@gmail.com>
2026-01-13 12:01:12 +0530

    chore: initial project structure and js variables practice

---
+++ learning/01-variables.js
@@ -0,0 +1,9 @@
+const Base_URL = "https://api.example.com/v1/";
+const USERNAME = "JohnDoe";
+const TIMEOUT = 5000;
+const welcomeMessageSelector = `//button[text()="Welcome, ${USERNAME}"]`;
+
+console.log("Base URL:", Base_URL);
+console.log("Username:", USERNAME);
+console.log("Timeout:", TIMEOUT);
+console.log("Welcome Message Selector:", welcomeMessageSelector);
